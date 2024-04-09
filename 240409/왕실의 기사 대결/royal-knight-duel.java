0db6c6e7366c0c7651a4498d029ac94cedab4c05
양소영 <48993188+YangSSo51@users.noreply.github.com>
2024-04-09 21:08:24 +0900

    Title: 왕실의 기사 대결, Time: 117ms, Memory: 9MB, Status: Passed - CodeTree

--- 240409/왕실의 기사 대결/royal-knight-duel.java
+++ 240409/왕실의 기사 대결/royal-knight-duel.java
@@ -10,7 +10,7 @@ public class Main {
     static int[] dx = {-1,0,1,0};
     static int[] dy = {0,1,0,-1};
     static Deque<Integer> canMoveKnights;
-    static int totalDamage;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -22,14 +22,15 @@ public class Main {
         board = new int[L+1][L+1];
         knightBoard = new int[L+1][L+1];
         knights = new Knight[N+1];
-//        canMoveKnights = new ArrayDeque<>();
-
+        int totalDamage = 0;
+        
         for (int i = 1; i <= L; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= L; j++) {
                 board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+        
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
 
@@ -39,31 +40,25 @@ public class Main {
             int w = Integer.parseInt(st.nextToken());
             int k = Integer.parseInt(st.nextToken());
             knights[i] = new Knight(i,r,c,h,w,k);
+            
             for (int j = r; j < r+h; j++) {
                 for (int l = c; l < c+w; l++) {
                     knightBoard[j][l] = i;
                 }
             }
         }
-//        for (int i = 1; i <= L; i++) {
-//            for (int j = 1; j <= L; j++) {
-//                System.out.print(knightBoard[i][j]+" ");
-//            }
-//            System.out.println();
-//        }
 
         for (int i = 0; i < Q; i++) {
             st = new StringTokenizer(br.readLine());
 
             int idx = Integer.parseInt(st.nextToken());
-            int direction = Integer.parseInt(st.nextToken());
-            moveKnight(idx,direction);
-//            System.out.println(moveKnight(idx,direction));
-            if(moveKnight(idx,direction)){
+            int direction = Integer.parseInt(st.nextToken());        
+
+            if(knights[idx].damage < knights[idx].k && moveKnight(idx,direction)){
                 damageKnight(idx,direction);
             }
         }
-//        System.out.println(totalDamage);
+       
         for(int i=1;i<=N;i++) {
         	if(knights[i].damage < knights[i].k) {
         		totalDamage+=knights[i].damage;
@@ -86,7 +81,6 @@ public class Main {
 
             for (int i = 0; i < size; i++) {
                 int now = Q.poll();
-//                System.out.println(now+" ");
                 int nx = knights[now].r+(dx[direction] > 0 ? knights[now].h : dx[direction]);
                 int ny = knights[now].c+(dy[direction] > 0 ? knights[now].w : dy[direction]);
                 int range = 0;
@@ -101,6 +95,7 @@ public class Main {
                             return false;
                         }
                         int nextKnight = knightBoard[nx][ny];
+                        
                         if (nextKnight > 0 && !isChecked[nextKnight] && knights[nextKnight].k > knights[nextKnight].damage) {
                             isChecked[nextKnight] = true;
                             Q.offer(nextKnight);
@@ -121,26 +116,26 @@ public class Main {
     private static void damageKnight(int idx, int direction) {
         while (!canMoveKnights.isEmpty()){
             int now = canMoveKnights.pollLast();
-            int nx = knights[now].r+(dx[direction] > 0 ? knights[now].h : dx[direction]);
-            int ny = knights[now].c+(dy[direction] > 0 ? knights[now].w : dy[direction]);
+            Knight nowKnight = knights[now];
 
-            int px = knights[now].r;
-            if(direction==0) px+=knights[now].h-1;
-            int py = knights[now].c;
-            if(direction==3) py += knights[now].w-1;
+            int nx = nowKnight.r+(dx[direction] > 0 ? nowKnight.h : dx[direction]);
+            int ny = nowKnight.c+(dy[direction] > 0 ? nowKnight.w : dy[direction]);
+
+            int px = nowKnight.r;
+            if(direction==0) px += nowKnight.h-1;
+            int py = nowKnight.c;
+            if(direction==3) py += nowKnight.w-1;
 
             int range = 0;
             //상,하일 때는 넓이로 비교
-            if(direction % 2 == 0) range = knights[now].w;
-            else range = knights[now].h;
-//            System.out.println("range : "+range);
+            if(direction % 2 == 0) range = nowKnight.w;
+            else range = nowKnight.h;
+
             for (int j = 0; j < range; j++) {
-                
-//                System.out.println("nx ny :"+nx+" "+ny);
-//                System.out.println("px py :"+px+" "+py);
 
                 knightBoard[nx][ny] = now;
                 knightBoard[px][py] = 0;
+                
                 if (direction % 2 == 0) {
                 	ny ++;
                 	py ++;
@@ -150,32 +145,30 @@ public class Main {
                 	px ++;
                 }
             }
-            knights[now].r += dx[direction];
-            knights[now].c += dy[direction];
-//            System.out.println(idx+" "+direction);
-//            for (int i = 1; i <= L; i++) {
-//                for (int j = 1; j <= L; j++) {
-//                    System.out.print(knightBoard[i][j]+" ");
-//                }
-//                System.out.println();
-//            }
-            //피해 더함
-            for (int i = knights[now].r; i < knights[now].r + knights[now].h; i++) {
-                for (int j = knights[now].c; j < knights[now].c+knights[now].w ; j++) {
+            nowKnight.r += dx[direction];
+            nowKnight.c += dy[direction];
+            
+            //피해 계산 
+            for (int i =  nowKnight.r; i < nowKnight.r + nowKnight.h; i++) {
+                for (int j = nowKnight.c; j < nowKnight.c+nowKnight.w ; j++) {
                     if(board[i][j] == 1){
-                        if(knights[now].damage < knights[now].k && idx!=now) {
-                            knights[now].damage++;
-//                            totalDamage++;
-//                            if(knights[now].damage == knights[now].k){
-//                            	totalDamage-= knights[now].k;
-//                            }
+                        if(idx!=now) {
+                        	nowKnight.damage++;                           
                         }
                     }
                 }
             }
+            
+            if(nowKnight.k <= nowKnight.damage) {
+            	for (int i =  nowKnight.r; i < nowKnight.r + nowKnight.h; i++) {
+                    for (int j = nowKnight.c; j < nowKnight.c+nowKnight.w ; j++) {
+                        knightBoard[i][j]=0;
+                    }
+                }
+            }
         }
-
     }
+    
     static boolean isRange(int x,int y){
         return x>= 1 && x <= L && y >= 1 && y <=L;
     }
@@ -187,6 +180,7 @@ public class Main {
          int w;
          int k;
          int damage;
+         
          Knight(int idx,int r,int c,int h,int w,int k){
              this.idx = idx;
              this.r = r;
